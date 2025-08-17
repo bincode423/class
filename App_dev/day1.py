9eb69df211d5c928c0170adfe793884a6c8dc0a8
Bincode423 <bincode423@gmail.com>
2025-08-17 10:28:47 +0900

    upload

--- App_dev/day1.py
+++ App_dev/day1.py
@@ -1,32 +1,27 @@
 import gradio as gr
 from ollama import chat
 
+# def answer(Question):
+#     prompt = "Your name is HIRO AI. Your company is HIRO AI."
+#     stream = chat(model="exaone3.5:7.8b", messages=[
+#         {"role": "system", "content": prompt},
+#         {"role": "user", "content": Question}
+#     ], stream=True)
+#     rt = ''
+#     for chunk in stream:
+#         rt += chunk.message.content
+#     return rt
 
-# prompt = f'When I give you a keyword, return exactly {size} unique sub-keywords related to it, separated by spaces. Do not include any explanations or extra text. Match the language of the input keyword.'
-# stream = chat(model="exaone3.5:7.8b", messages=[
-#     {"role": "system", "content": prompt},
-#     {"role": "user", "content": option}
-# ], stream=True)
-
-# rt = ''
-# for chunk in stream:
-#     rt += chunk.message.content
-# new_keywords = set(rt.split())
-
-def answer(Question):
-    prompt = "Your name is HIRO AI. Your company is HIRO AI."
-    stream = chat(model="exaone3.5:7.8b", messages=[
-        {"role": "system", "content": prompt},
-        {"role": "user", "content": Question}
-    ], stream=True)
-    rt = ''
-    for chunk in stream:
-        rt += chunk.message.content
-    return rt
+def smart_hello(name):
+    if name == '':
+        return "이름을 입력하세요."
+    else:
+        return f"안녕하세요, {name}님!"
 
 demo = gr.Interface(
-    fn=answer,
-    inputs=gr.Textbox(lines=2, placeholder="Ask me anything"),
+    fn=smart_hello,
+    inputs=gr.Textbox(lines=2, placeholder="이름을 입력하세요."),
     outputs=gr.Textbox()
 )
 demo.launch()
+
