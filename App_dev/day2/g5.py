89ef4273bddd687e2be641684ab5a12db08e5bba
Bincode423 <bincode423@gmail.com>
2025-08-24 11:04:32 +0900

    day_2_3

---
+++ App_dev/day2/g5.py
@@ -0,0 +1,11 @@
+import gradio as gr
+import pyttsx3
+import tempfile
+import random
+from datetime import datetime
+
+def ai_assistant(user_text):
+    pass
+
+demo = gr.Interface(
+    inputs = gr.Textbox(label="User Input"),
